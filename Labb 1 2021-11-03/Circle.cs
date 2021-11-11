bf07afff704e7bab195393304673bf25ccb39b6e
Filip <Filip@FILIP-DATOR>
2021-11-11 13:04:25 +0100

    Add project files.

---
+++ Labb 1 2021-11-03/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb_1_2021_11_03
+{
+    public class Circle
+    {
+        public float _pi = 3.141f;
+        public int _Radius;  
+        
+        public Circle(int radie)
+        {
+            this._Radius = radie;
+
+        }
+
+        public float getArea()
+        {
+            float result =_Radius * _Radius *_pi;
+            return result;
+
+        }
+    }
+}
---
+++ Labb 1 2021-11-03/Labb 1 2021- 11-11 Klar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Labb_1_2021_11_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+             
+
+            Circle Cir1 = new Circle(5);
+            Circle Cir2 = new Circle(6);
+
+            Console.WriteLine("Arean på en cirkel med radie" + Cir2._Radius + " är " + Cir2.getArea());
+            Console.WriteLine("Arean på en cirkel med radie" + Cir1._Radius + " är " + Cir1.getArea());
+
+
+
+        }
+    }
+}
